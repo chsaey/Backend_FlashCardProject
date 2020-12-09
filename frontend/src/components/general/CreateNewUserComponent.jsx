3642ad2df324a3f0045183438e98ff387ba3e59d
Dan Fellows <dfellows68@gmail.com>
2020-12-09 13:38:21 -0600

    Move changes from SignIn to CreateNewUser/Add Formik/Edit UserDataServic

--- frontend/src/components/general/CreateNewUserComponent.jsx
+++ frontend/src/components/general/CreateNewUserComponent.jsx
@@ -1,10 +1,76 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
+import UserDataService from '../../services/UserDataService';
+
 
 class CreateNewUserComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            username: '',
+            password: ''
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(values) {
+        let user = {
+            id: this.state.id,
+            firstName: values.firstName,
+            lastName: values.lastName,
+            username: values.username,
+            password: values.password
+        }
+
+        UserDataService.updateUser(user)
+        .then(() => this.props.history.push('/UserRegistry'))
+    }
+
     render() {
+        let {id, firstName, lastName, username, password} = this.state
         return (
             <div className="container">
-                <p>TEST</p>
+                <br />
+                <br />
+                <div className="jumbotron" style={{backgroundColor:"Black"}}>
+                    <h2 style={{color:"Green"}}>Add your info...</h2>
+                    <br />
+                    <Formik
+                        initialValues={{id, firstName, lastName, username, password}}
+                        onSubmit={this.onSubmit}
+                        enableReinitialize={true}
+                    >
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>ID:</label>
+                                        <Field className="form-control" type="text" name="id" disabled />
+                                    </fieldset>
+                                    <fieldset>
+                                        <label>First Name:</label>
+                                        <Field className="form-control" type="text" name="firstName" />
+                                    </fieldset>
+                                    <fieldset>
+                                        <label>Last Name:</label>
+                                        <Field className="form-control" type="text" name="lastName" />
+                                    </fieldset>
+                                    <fieldset>
+                                        <label>Username:</label>
+                                        <Field className="form-control" type="text" name="username" />
+                                    </fieldset>
+                                    <fieldset>
+                                        <label>Password:</label>
+                                        <Field className="form-control" type="text" name="password" />
+                                    </fieldset>
+                                </Form>
+                            )
+                        }
+                        </Formik>
+                </div>
             </div>
         )
     }
--- frontend/src/components/general/SignInComponent.jsx
+++ frontend/src/components/general/SignInComponent.jsx
@@ -1,51 +1,10 @@
 import React, { Component } from 'react';
 
 class SignInComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        alert('Your data was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
-
     render() {
         return (
             <div className="container">
-                <br />
-                <br />
-                <div className="jumbotron" style={{backgroundColor:"Black"}}>
-                    <form>
-                        <h2 style={{color:"Green"}}>Add your info...</h2>
-                        <br />
-                        <h6 style={{color:"white", textAlign:"left"}}>First Name:</h6>
-                        <input type="text" firstName="firstName" style={{float:"left"}} />
-                        <br />
-                        <br />
-                        <h6 style={{color:"white", textAlign:"left"}}>Last Name:</h6>
-                        <input type="text" lastName="lastName" style={{float:"left"}} />
-                        <br />
-                        <br />
-                        <h6 style={{color:"white", textAlign:"left"}}>Username:</h6>
-                        <input type="text" username="username" style={{float:"left"}} />
-                        <br />
-                        <br />
-                        <h6 style={{color:"white", textAlign:"left"}}>Password:</h6>
-                        <input type="text" password="password" style={{float:"left"}} />
-                        <br />
-                        <br />
-                        <button type="submit" style={{float:"left"}}>Submit</button>
-                    </form>    
-                </div>
+                <p>Test</p>
             </div>
         )
     }
